b7cfca9520db3b3a13b33612d39af2eb3bb94e27
mottox2 <y.mottox2@gmail.com>
2020-02-20 19:08:16 +0900

    Tweek styles

--- src/components/Tweet.tsx
+++ src/components/Tweet.tsx
@@ -1,12 +1,27 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core'
 import css from '@emotion/css/macro'
+import TextareaAutosize from 'react-autosize-textarea';
 
 const Tweet: React.FC<{
   tweet: string
   onUpdate: (tweet: string) => void
-}> = ({ tweet, onUpdate }) => <textarea onChange={(e) => {
-  onUpdate(e.target.value)
-}} css={css`font-size: 12px;`} value={tweet}></textarea>
+}> = ({ tweet, onUpdate }) => <TextareaAutosize onChange={(e) => {
+  console.log((e.target as any).value)
+  onUpdate((e.target as any).value)
+}} css={css`
+  display: block;
+  width: 100%;
+  padding: 16px;
+  border-bottom: 1px solid #ddd;
+  font-size: 16px;
+
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.04);
+  }
+  &:focus {
+    background-color: rgba(0, 0, 0, 0.04);
+  }
+`} value={tweet}/>
 
 export default Tweet
